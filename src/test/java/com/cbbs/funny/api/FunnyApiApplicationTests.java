bc35c646b574a98a67f59c9464df458b3bcb5bce
Chawalit Rungkasemsuk <chawalit_r@yahoo.com>
2020-10-10 11:57:49 +0700

    Project initialization

---
+++ src/test/java/com/cbbs/funny/api/FunnyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cbbs.funny.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunnyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
